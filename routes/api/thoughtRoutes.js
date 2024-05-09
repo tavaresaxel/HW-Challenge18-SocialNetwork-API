ce478c471bd2f681187025a342b69fc62922de65
axel tavares <tavaresaxel95@gmail.com>
2024-05-08 20:11:55 -0400

    upated fuctions to match new controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const {
-  getVideos,
-  getSingleVideo,
-  createVideo,
-  updateVideo,
-  deleteVideo,
-  addVideoResponse,
-  removeVideoResponse,
-} = require('../../controllers/videoController');
+  getThoughts,
+  getSingleThoughts,
+  createThoughts,
+  updateThoughts,
+  deleteThoughts,
+  addThoughtReaction,
+  removeThoughtReaction,
+} = require('../../controllers/thoughtController');
 
 // /api/videos
 //http://localhost:3001/api/videos
-router.route('/').get(getVideos).post(createVideo);
+router.route('/').get(getThoughts).post(createThoughts);
 
 // /api/videos/:videoId
 router
-  .route('/:videoId')
-  .get(getSingleVideo)
-  .put(updateVideo)
-  .delete(deleteVideo);
+  .route('/:thoughtsId')
+  .get(getSingleThoughts)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
 
 // /api/videos/:videoId/responses
 //http://localhost:3001/api/videos/
-router.route('/:videoId/responses').post(addVideoResponse);
+router.route('/:thoughtsId/reactions').post(addThoughtReaction);
 
 // /api/videos/:videoId/responses/:responseId
-router.route('/:videoId/responses/:responseId').delete(removeVideoResponse);
+router.route('/:videoId/reactions/:reactionId').delete(removeThoughtReaction);
 
 module.exports = router;
